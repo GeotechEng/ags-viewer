323c5e27425f2b22944da03c278879fe55cfe566
Shawn <69837734+GeotechEng@users.noreply.github.com>
2024-02-04 07:24:16 +0000

    changed structure of app and added ags pages

---
+++ pages/1_Input_Page.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit.logger import get_logger
+import numpy as np
+import pandas as pd
+from python_ags4 import AGS4
+
+st.set_page_config(
+    page_title="AGS4 Input Page",
+    page_icon="👋",
+)
+
+st.write("# AGS4 Input Page")
+
+st.markdown(
+    """
+Notes 
+- The tool app works with AGS4 files only 
+- select an AGS4 file using the file selection button 
+- Select the headers within the file that contain the Eastings, Northings or Latitude and Longitude
+- Select the Coordinate Reference System (CRS) for the coordinates the uses
+- Select the header containing the elevation 
+- Run the file, results are presented in the Report Page tab 
+- An AGS check report is run as well to identify any errors or warnings, this is reported on the Check Page tab
+"""
+)
